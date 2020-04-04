97d7213b0f4ecd8327bcbda782174434753dc8ec
rhishikesh <rhishikesh@helpshift.com>
2020-04-04 12:18:27 +0530

    Initial Commit

---
+++ AntsColony/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AntsColony
+//
+//  Created by rhishikesh on 04/04/20.
+//  Copyright © 2020 Helpshift. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
